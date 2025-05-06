79237a39461eb3b19c7c6d32d87d8e01c5cb5be7
Oomass7 <109863567+Oomass7@users.noreply.github.com>
2025-05-06 15:10:03 -0500

    Update ActivityCajero.py
    Codigo terminado junto con sus validaciones

--- ActivityCajero.py
+++ ActivityCajero.py
@@ -8,49 +8,101 @@
 
 
 
+usersAcount = [{"nombre": "Tomas",
+                "Account": "123456789",
+                "saldo": 200000},
+                
+               {"nombre": "Andres",
+                "Account": "987654321", 
+                "saldo": 3000000},
 
-usersAcount = []
+               {"nombre": "David",
+                "Account": "456789123",
+                "saldo": 500000}]
 
 while True:
     print("-"*50)
     print("MENU \n1. Depositar \n2. Retirar \n3. Mostrar Saldo \n4. Salir")
     print("-"*50)
-    try:
-        option = input("Digite la opcion que desea: ")
-    except:
-        print("Por favor digite solo el numero de la opcion")
+    option = int(input("Digite la opcion que desea: "))
         
-    while True:
-        match option:
-            case 1:
-                print("-"*50)
-                print("DEPOSITAR")
-                print("-"*50)
-                
-                name = input("Ingresa tu nombre: ")
-                if name not in usersAcount:
-                    number = input("")
-                
-                
-            case 2:
-                print("-"*50)
-                print("RETIRAR")
-                print("-"*50)
-                
-            case 3:
-                print("-"*50)
-                print("MOSTRAR SALDO")
-                print("-"*50)
-                
-            case 4:
-                print("Gracias por utilizar el programa")
-            
-            case _:
-                print("Digite una opcion que este en el menu")
-                
 
-    
-    
+    match option:
+        case 1:
+            print("-"*50)
+            print("DEPOSITAR")
+            print("-"*50)
+            number = (input("Ingrese su numero de cuenta: "))
+            for i in usersAcount:
+                if i["Account"] == number:
+                    print("Bienvenido", i["nombre"])
+                    try:
+                        deposit = int(input("Cuanto desea depositar: "))
+                        if deposit <= 0:
+                            print("El monto a depositar no puede ser menor o igual a 0")
+                            break
+                        else:
+                            i["saldo"] += deposit
+                            print("Deposito exitoso")
+                            break
+                    except:
+                        print("Por favor digite solo numeros")
+                        break
+                else:
+                    print("No existe una cuenta con ese numero o ingresaste mal el numero")
+                    break
+                
+        
+        case 2:
+            print("-"*50)
+            print("RETIRAR")
+            print("-"*50)
+            number = (input("Ingrese su numero de cuenta: "))
+            for i in usersAcount:
+                if i["Account"] == number:
+                    if  i["saldo"] <= 0:
+                        print("No tiene saldo en la cuenta")
+                        break
+                    else:
+                        print("Bienvenido", i["nombre"])
+                        try:
+                            withdraw = int(input("Cuanto deseas retirar: "))
+                            if withdraw > i["saldo"]:
+                                print("No tiene suficiente saldo")
+                                break
+                            else:
+                                i["saldo"] -= withdraw
+                                print("Retiro exitoso")
+                                break
+                        except:
+                            print("Por favor digite solo numeros")
+                            break
+                else:
+                    print("No existe una cuenta con ese numero o ingresaste mal el numero")
+                    break
+            
+        case 3:
+            print("-"*50)
+            print("MOSTRAR SALDO")
+            print("-"*50)
+            number = (input("Ingrese su numero de cuenta: "))
+            for i in usersAcount:
+                if i["Account"] == number:
+                    print("Bienvenido", i["nombre"])
+                    print("Su saldo es de: ", i["saldo"])
+                    break
+                else:
+                    print("No existe una cuenta con ese numero o ingresaste mal el numero")
+                    break
+            
+        case 4:
+            print("Gracias por utilizar el programa")
+            break
+        
+        case _:
+            print("Digite una opcion que este en el menu")
+    exit           
+                  
     
     
     
